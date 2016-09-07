b83bbcd917e6aceab1d40585778f0c88960fcb6c
unknown <C:\The Bat>
2016-09-08 01:01:26 +0300

    final

--- markup/js/main.js
+++ markup/js/main.js
@@ -17,3 +17,27 @@ $(function(){
 	
 })
 
+
+createNovigation("#main-nav");
+
+createNovigation("#portfolio-nav");
+
+function createNovigation(id)
+{
+	var nav = $(id);
+	$("<select class='nav-select visible-xs' />").appendTo(nav);
+	var select = nav.find("select");
+
+	// Populate dropdown with menu items
+	nav.find("a").each(function() {
+		var el = $(this);
+			$("<option />", {
+			"value"   : el.attr("href"),
+			"text"    : el.text()
+		}).appendTo(id + " select");
+	});
+
+	$(id + " select").change(function() {
+		window.location = $(this).find("option:selected").val();
+	});
+}
